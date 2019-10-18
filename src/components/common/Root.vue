0c1d123378e98c5c7cff7cf5e55e3b9180a1daaa
gq <guanqinghz@163.com>
2019-10-18 23:46:12 +0800

    提交root

--- src/components/common/Root.vue
+++ src/components/common/Root.vue
@@ -1,13 +1,41 @@
 <template>
-
+  <div id="root" style="height: 100%">
+    <view-box ref="viewBox">
+      <div>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/></div>
+    </view-box>
+    <tabbar slot="bottom" style="position: fixed">
+      <tabbar-item>
+        <img slot="icon" src="../../assets/icon-home.png"/>
+        <img slot="icon-active" src="../../assets/icon-home-active.png"/>
+        <p slot="label">首页</p>
+      </tabbar-item>
+      <tabbar-item>
+        <img slot="icon" src="../../assets/icon-home.png"/>
+        <img slot="icon-active" src="../../assets/icon-home-active.png"/>
+        <p slot="label">个人中心</p>
+      </tabbar-item>
+    </tabbar>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "root"
-    }
+import { ViewBox, Tabbar, TabbarItem } from 'vux'
+import Tab from "vux/src/components/tab/tab";
+export default {
+  name: "root",
+  components: {
+    Tab,
+    ViewBox,
+    Tabbar,
+    TabbarItem
+  }
+}
 </script>
 
 <style scoped>
-
+html, body {
+  height: 100%;
+  width: 100%;
+  overflow-x: hidden;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Root from '@/components/common/Root'
 
 Vue.use(VueRouter)
 
@@ -8,8 +8,8 @@ export default new VueRouter({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Root',
+      component: Root
     }
   ]
 })
