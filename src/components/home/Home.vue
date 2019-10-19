d6311fcc238f49b260aa753e2a447ef5c3a0a887
gq <guanqinghz@163.com>
2019-10-19 23:23:27 +0800

    完成首页轮播

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,12 +1,60 @@
 <template>
   <div id="home">
-      this is home
+      <search
+        v-model="searchText"
+        placeholder="请输入关键字"
+        auto-scroll-to-top
+        @on-submit="beginSearch"
+        @on-cancel="cancel"
+      />
+      <swiper
+        :list="swiperList"
+        auto
+        loop
+        :aspect-ratio = 0.4
+      />
+      <div>aaa</div>
+    <div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div><div>aaa</div>
   </div>
 </template>
 
 <script>
+import { Search, Swiper } from 'vux'
 export default {
-  name: "home"
+  name: "home",
+  data(){
+    return {
+      searchText: '', //搜索框内容
+      swiperList: [] //轮播数组
+    }
+  },
+  created(){
+    this.$axios.get('/homepage/getHomePageList')
+      .then(res => {
+        console.log(JSON.stringify(res))
+        this.swiperList = res.data.data.map(item =>{
+          return {
+            url: '/PersonCenter',
+            img: item.fileRealPath,
+            title: item.title
+          }
+        })
+        console.log(this.swiperList)
+      })
+      .catch(err => console.log(err))
+  },
+  methods: {
+    beginSearch: function (value) {
+      alert('beginSearch:'+value+'searchText:'+this.searchText)
+    },
+    cancel: function () {
+      alert(this.searchText)
+    }
+  },
+  components: {
+    Search,
+    Swiper
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import FastClick from 'fastclick'
 import router from './router'
 import App from './App'
 
+// Axios
+import Axios from 'axios'
+Axios.defaults.baseURL = 'https://wx.hongyancloud.com/wxDev'
+Vue.prototype.$axios = Axios
+
+import FastClick from 'fastclick'
 FastClick.attach(document.body)
 
 Vue.config.productionTip = false
